3f4e4c3ff4f8ce71df754af19633174c336d6c4b
Lucas Guzmán-Finn <lggufi@gmail.com>
2020-07-29 00:32:11 -0400

    CORS error splitting into teams, deploying w/ list

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -19,6 +19,16 @@ const useStyles = theme => ({
       color: 'white',
       background: theme.palette.primary.dark,
     },
+    barPaperComplete: {
+        padding: theme.spacing(2),
+        margin: theme.spacing(2),
+        display: 'flex',
+        flexDirection: 'row',
+        overflow: 'hidden',
+        whiteSpace: 'nowrap',
+        color: 'white',
+        background: 'green',
+      },
     name: {
       flexGrow: 1,
     },
@@ -62,11 +72,17 @@ class Leaderboard extends Component {
     };
 
     Bar(props) {
-        const { name, points, classes, maxPoints } = props;
-        const width = points / maxPoints;
+        const { name, points, classes } = props;
+        const width = (points > 1000) ? 1 : (points < 100) ? 0.1 : points / 1000;
+        
+        let style = classes.barPaper;
+        if (points >= 1000) {
+            style = classes.barPaperComplete;
+        }
+
         return(
             <Box width={width}>
-                <Paper elevation={3} className={classes.barPaper}>
+                <Paper elevation={3} className={style}>
                     <Typography className={classes.name}> {name} </Typography>
                     <Typography> {points} </Typography>
                 </Paper>
@@ -86,7 +102,6 @@ class Leaderboard extends Component {
     render() {
         const { interns, error, loading } = this.state;
         const { classes } = this.props;
-        let maxPoints = 404;
 
         if (error) {
             console.log(error);
@@ -94,18 +109,20 @@ class Leaderboard extends Component {
         }
 
         let data = [{name: "ERROR", points: 404}];
+        // let team1 = [{name: "ERROR", points: 404}];
+        // let team2 = [{name: "ERROR", points: 404}];
+        // let team3 = [{name: "ERROR", points: 404}];
+        // let team4 = [{name: "ERROR", points: 404}];
 
         let loadingStyle = classes.loading;
         if(!loading) {
             loadingStyle = classes.notLoading;
             data = interns;
             data.sort(this.comparePoints);
-            maxPoints = data[0].points;
-            console.log(data);
-            // let team1 = data.slice(0, 10).sort(this.comparePoints);
-            // let team2 = data.slice(10, 21).sort(this.comparePoints);
-            // let team3 = data.slice(21, 32).sort(this.comparePoints);
-            // let team4 = data.slice(32, 41).sort(this.comparePoints);
+            // team1 = data.slice(0, 10).sort(this.comparePoints);
+            // team2 = data.slice(10, 21).sort(this.comparePoints);
+            // team3 = data.slice(21, 32).sort(this.comparePoints);
+            // team4 = data.slice(32, 41).sort(this.comparePoints);
         }
 
         return (
@@ -117,7 +134,7 @@ class Leaderboard extends Component {
                     <Fade in={!loading} timeout={1000}>
                         <div>
                             {data.map((intern, key) => (
-                                <this.Bar key={key} name={intern.name} points={intern.points} classes={classes} maxPoints={maxPoints}/>
+                                <this.Bar key={key} name={intern.name} points={intern.points} classes={classes}/>
                             ))}
                         </div>
                     </Fade>
