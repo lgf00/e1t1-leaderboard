8948930bece0c895ab0bb23f50e223dc417285f5
Lucas Guzmán-Finn <lggufi@gmail.com>
2020-08-05 22:43:04 -0400

    updated (part 2) for new week

--- src/pages/CurrentWeek.jsx
+++ src/pages/CurrentWeek.jsx
@@ -5,7 +5,7 @@ import Leaderboard from '../components/Leaderboard'
 export default function CurrentWeek(){
     return (
         <div>
-          <Header name="Week 8/2"/>
+          <Header name="Week 8/9"/>
           <Leaderboard/>
         </div>
     );
